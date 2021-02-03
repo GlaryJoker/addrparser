6cee8bac8964e51eebd0f4b99521a63c296551f4
MengXiaocheng <mengxiaocheng@gammainfo.com>
2021-02-03 17:37:44 +0800

    只暴露getAll方法

--- app/Extract.php
+++ app/Extract.php
@@ -4,7 +4,7 @@ namespace Addrparser;
 
 class Extract
 {
-    protected  $address = '';
+    protected $address = '';
 
     private $provinces;
     private $cities;
@@ -16,6 +16,12 @@ class Extract
 
     protected $resultCounties = [];
 
+    protected $countyConfirmed = false;
+
+    protected $cityConfirmed = false;
+
+    protected $provinceConfirmed = false;
+
     public function __construct()
     {
         $this->provinces = Dict::getProvinces();
@@ -23,18 +29,155 @@ class Extract
         $this->counties = Dict::getCounies();
     }
 
-    public function setAddress($address){
+    public function setAddress($address)
+    {
         $this->address = $address;
         return $this;
     }
+
+
+    /**
+     * 获取区或者县
+     * @author www.iplayio.cn
+     * @since 2021/1/25 10:02
+     */
+    protected function parseCounty()
+    {
+
+        foreach ($this->counties as $county) {
+            $keywords = explode('/', $county->keywords);
+            $kwLength = count($keywords);
+            for ($i = 0; $i < $kwLength; $i++) {
+                if (preg_match('/' . $keywords[$i] . '/', $this->address)) {
+                    //如果匹配到高新两个字
+                    $county->score = $i;
+                    $this->resultCounties[] = $county;
+                    break;
+                }
+            }
+        }
+
+        $this->resultCounties = $this->removeDuplicate($this->resultCounties);
+        if(count($this->resultCounties) === 1){
+            $this->countyConfirmed = true;
+            //如果只有一个区县，则只获取城市、province就就行了
+
+            $this->resultCities = [Finder::getCityByCode($this->resultCounties[0]->cityCode)];
+            $this->resultProvinces = [Finder::getProvinceByCode($this->resultCities[0]->provinceCode)];
+
+            $this->cityConfirmed = true;
+            $this->provinceConfirmed = true;
+            return $this;
+        }
+
+        //
+
+        foreach ($this->resultCounties as $county) {
+
+            if (preg_match('/高新/', $county->name)) {
+                $tmpName = str_replace('高新技术产业开发区', '', $county->name);
+                //获取该城市名字
+                $relatedCities = Finder::searchCity($tmpName);
+                foreach ($relatedCities as $cityIndex => $relatedCity) {
+                    $keywords = explode('/', $relatedCity->keywords);
+                    foreach ($keywords as $keyword) {
+                        if (preg_match('/' . $keyword . '/', $this->address)) {
+                            $theCity = $relatedCity;
+                            $this->resultCounties = [$county];
+                            $this->resultCities = [$theCity];
+                            $this->countyConfirmed = true;
+                            $this->cityConfirmed = true;
+                            break;
+                        }
+                    }
+                    if (isset($theCity)) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        if($this->countyConfirmed && $this->cityConfirmed){
+
+            $this->provinceConfirmed = true;
+            $theProvince = Finder::getProvinceByCode($theCity->provinceCode);
+            $this->resultProvinces = [$theProvince];
+            return $this;
+        }
+
+        //获取到区县，然后获得市，如果省份为空则获得省份。
+        /**
+         * 4个直辖市
+         */
+        foreach ($this->resultCounties as $county) {
+            if ($county->cityCode === '3101' || $county->cityCode === '1101'
+                || $county->cityCode === '5001'
+                || $county->cityCode === '1201'
+            ) {
+                $this->resultCities[] = Finder::getProvinceByCode($county->provinceCode);
+            } else {
+                $this->resultCities[] = Finder::getCityByCode($county->cityCode);
+            }
+        }
+
+        foreach ($this->resultCities as $city) {
+            $this->resultProvinces[] = Finder::getProvinceByCode($city->cityCode);
+        }
+
+        return $this;
+    }
+
+    /**
+     * 获取城市
+     * @author www.iplayio.cn
+     * @since 2021/1/25 10:02
+     */
+    protected function parseCity()
+    {
+        if($this->cityConfirmed){
+            return $this;
+        }
+
+        foreach ($this->cities as $city) {
+            $keywords = explode('/', $city->keywords);
+            $kwLength = count($keywords);
+            for ($i = 0; $i < $kwLength; $i++) {
+                if (preg_match('/' . $keywords[$i] . '/', $this->address)) {
+                    $city->score = $i;
+                    $this->resultCities[] = $city;
+                    break;
+                }
+            }
+        }
+        $this->resultCities = $this->removeDuplicate($this->resultCities);
+
+        if(count($this->resultCities) === 1){
+            $this->cityConfirmed = true;
+            $this->provinceConfirmed = true;
+            $this->resultProvinces = [Finder::getProvinceByCode($this->resultCities[0]->provinceCode)];
+            return $this;
+        }
+
+        foreach ($this->resultCities as $city) {
+            $this->resultProvinces[] = Finder::getProvinceByCode($city->provinceCode);
+        }
+
+        return $this;
+    }
+
 
     /**
      * 获取省份
      * @author www.iplayio.cn
      * @since 2021/1/25 10:02
      */
-    public  function parseProvince()
+    protected function parseProvince()
     {
+
+        if($this->provinceConfirmed || $this->cityConfirmed || $this->countyConfirmed){
+            return $this;
+        }
+
         foreach ($this->provinces as $province) {
             $names = explode('/', $province->keywords);
             for ($i = 0; $i < count($names); $i++) {
@@ -47,176 +190,66 @@ class Extract
             }
         }
         $this->resultProvinces = $this->removeDuplicate($this->resultProvinces);
+        if(count($this->resultProvinces) === 1){
+            $this->provinceConfirmed = true;
+        }
         return $this;
     }
 
-    /**
-     * 获取城市
-     * @author www.iplayio.cn
-     * @since 2021/1/25 10:02
-     */
-    public  function parseCity()
+
+    public function getAll()
     {
-
-        foreach ($this->cities as $city){
-            $keywords = explode('/',$city->keywords);
-            $kwLength = count($keywords);
-            for($i=0;$i<$kwLength;$i++){
-                if(preg_match('/'.$keywords[$i].'/',$this->address)){
-                    $city->score = $i;
-                    $this->resultCities[] = $city;
-                    break;
-                }
-            }
-        }
-        $this->resultCities = $this->removeDuplicate($this->resultCities);
-        foreach ($this->resultCities as $city){
-            $this->resultProvinces[] = Finder::getProvinceByCode($city->provinceCode);
-        }
-
-        return $this;
-    }
-
-    /**
-     * 获取区或者县
-     * @author www.iplayio.cn
-     * @since 2021/1/25 10:02
-     */
-    public  function parseCounty()
-    {
-
-        foreach ($this->counties as $county){
-            $keywords = explode('/',$county->keywords);
-            $kwLength = count($keywords);
-            for($i=0;$i<$kwLength;$i++){
-                if(preg_match('/'.$keywords[$i].'/',$this->address)){
-                    //如果匹配到高新两个字
-                    $county->score = $i;
-                    $this->resultCounties[] = $county;
-                    break;
-                }
-            }
-        }
-
-        $this->resultCounties = $this->removeDuplicate($this->resultCounties);
-
-        //
-
-
-        foreach ($this->resultCounties as $county){
-
-            if(preg_match('/高新/',$county->name)){
-                $tmpName = str_replace('高新技术产业开发区','',$county->name);
-                //获取该城市名字
-                $relatedCities = Finder::searchCity($tmpName);
-
-                foreach ($relatedCities as $relatedCity){
-
-                }
-
-                //如果等于1，则获取到该城市
-                $tmpCityName = str_replace('市','',$relatedCities[0]->name);
-                $countyFullName = $tmpCityName."高新技术产业开发区";
-                $theCounty = Finder::getCountyByName($countyFullName);
-                $tmpCounties[] = $theCounty;
-            }
-        }
-
-        if(isset($tmpCounties)){
-            $this->resultCounties = $tmpCounties;
-        }
-
-
-        //获取到区县，然后获得市，如果省份为空则获得省份。
-        /**
-         * 4个直辖市
-         */
-        foreach ($this->resultCounties as $county){
-            if($county->cityCode === '3101' || $county->cityCode === '1101'
-                || $county->cityCode === '5001'
-                || $county->cityCode === '1201'
-            ){
-                $this->resultCities[] = Finder::getProvinceByCode($county->provinceCode);
-            }else{
-                $this->resultCities[] = Finder::getCityByCode($county->cityCode);
-            }
-        }
-
-        foreach ($this->resultCities as $city){
-            $this->resultProvinces[] = Finder::getProvinceByCode($city->cityCode);
-        }
-
-        return $this;
-    }
-
-    public function parseAll(){
-        //解析县区
         $this->parseCounty();
         $this->parseCity();
         $this->parseProvince();
-        return $this;
-    }
 
-    /**
-     * 处理
-     * @author www.iplayio.cn
-     * @since 2021/2/2 12:54
-     */
-    public function getAll(){
-
-        return [
-            'provinces' => $this->getProvince(),
-            'cities' => $this->getCity(),
-            'counties' => $this->getCounty()
+        $result = [
+            'provinces' => $this->resultProvinces,
+            'cities' => $this->resultCities,
+            'county' => $this->resultCounties
         ];
-    }
 
-    public function getCity(){
+        $countyConfirmed = $this->countyConfirmed;
+        $cityConfirmed = $this->cityConfirmed;
+        $provinceConfirmed = $this->provinceConfirmed;
 
-        $cities = $this->removeDuplicate($this->resultCities);
+        $this->countyConfirmed = false;
+        $this->cityConfirmed = false;
+        $this->provinceConfirmed = false;
+
         $this->resultCities = [];
-
-        return $cities;
-    }
-
-    public function getCounty(){
-        $counties = $this->resultCounties;
-        $this->resultCounties = [];
-        return $counties;
-    }
-
-    public function getProvince(){
-         $provinces = $this->resultProvinces;
         $this->resultProvinces = [];
-         return $provinces;
+        $this->resultCounties = [];
+        return $result;
     }
 
-    protected function removeDuplicate(array $array = []){
-        if(!$array){
+    protected function removeDuplicate(array $array = [])
+    {
+        if (!$array) {
             return [];
         }
 
-        if(count($array) === 1){
+        if (count($array) === 1) {
             return $array;
         }
 
-        $cityCodes = array_unique(array_column($array,'code'));
+        $cityCodes = array_unique(array_column($array, 'code'));
         $exitsCode = [];
         $resultArray = [];
 
         //先获取score为0的，然后去除其他的，如果没有为0的，返回其他匹配到数字
-        $filterArray = array_filter($array,function($item){
+        $filterArray = array_filter($array, function ($item) {
             return $item->score === 0;
         });
 
         $rCount = count($filterArray);
-        if($rCount === 1){
+        if ($rCount === 1) {
             return $filterArray;
         }
 
-        foreach ($array as $city){
-            if(in_array($city->code,$cityCodes) && !in_array($city->code,$exitsCode)){
-                array_push($exitsCode,$city->code);
+        foreach ($array as $city) {
+            if (in_array($city->code, $cityCodes) && !in_array($city->code, $exitsCode)) {
+                array_push($exitsCode, $city->code);
                 $resultArray[] = $city;
                 continue;
             }
@@ -225,7 +258,8 @@ class Extract
         return $resultArray;
     }
 
-    public function getAddress(){
+    public function getAddress()
+    {
         return $this->address;
     }
 
